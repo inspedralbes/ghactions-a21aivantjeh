36e1ac01bee69b794804566da5b9f5d4af362132
Aivan Antonio <a21aivantjeh@inspedralbes.cat>
2025-04-13 22:03:38 +0200

    Calculadora (Vue.JS) creada

---
+++ front/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4 flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
+    <p class="text-3xl font-bold border-b border-gray-400 text-gray-500">Calculadora</p>
+    <Calculator />
+    <p class="text-xs text-gray-400">Aivan Antonio</p>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculadora.vue'
+
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
---
+++ front/src/components/Calculadora.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="min-w-sm p-4 bg-white rounded-lg shadow-md">
+        <div class="bg-gray-100 p-4 mb-4 text-center rounded-md text-gray-600 border-2 border-gray-300">
+            {{ message }}
+        </div>
+
+        <div class="grid grid-cols-3 gap-2">
+            <button v-for="number in 9" :key="number" @click="handleClick(number)"
+                class="bg-gray-500 hover:bg-gray-600 text-white py-3 rounded-md text-lg border-4 border-gray-700/30">
+                {{ number }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Pulsa un número'
+        }
+    },
+    methods: {
+        handleClick(number) {
+            this.message = `Has pulsado el número ${number}`;
+        }
+    }
+}
+</script>
